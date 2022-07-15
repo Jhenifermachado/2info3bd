578eb5945a906acb0cabb1566651f6564fb676d5
jhenifer <jhenifermachado211@gmail.com>
2022-07-15 15:08:57 -0300

    master

---
+++ banco_2info3.sql
@@ -0,0 +1,31 @@
+insert into clientes(nome, email, login, senha, endereco_cobranca) values
+("Julia Ribeiro","julia.ribeiro@gmail.com","julia.ribeiro","12@#$d83j56","Rua dos Bla78364 23"),
+("Rose Nascimento","rose@gmail.com","rose.nascimento","12@#$0osl9","Rua dos blablabla 89723"),
+("Daniela da Silva","daniela.silva@gmail.com","dani.silva","12@#$9skw4","Rua dos Comerciarios 90"); 
+
+insert into produtos (qtd_embalagem,preco_embalagem,qtd_estoque,desc_produto,preco_unitario) values
+(1,7580.35,NULL,"Notebook Lenovo",7580.35),
+(1,258.25,NULL,"Teclado DELL com fio",258.25),
+(1,127.50,NULL,"Mouse DELL com fio",127.50),
+(1,1235.48,NULL,"Cadeira Gamer",1235.48),
+(1,875.98,NULL,"Monitor DELL FULL HD",875.98),
+(1,875.93,NULL,"Mesa esritório 150x85",875.93),
+(1,2578.34,NULL,"Projetor SONY 4K 120HZ",2578.34),
+(1,120.36,NULL,"AVAST",120.36),
+(1,425.38,NULL,"WINDOWS 11",425.38),
+(1,1890.35,NULL,"Celular XIAOMI POCO3",1890.35);
+
+insert into comentario(cod_cliente,cod_produto,texto,titulo)
+1 , Maria Ribeiro   , ribeiro.maria@gmail.com , maria.ribeiro   , 12@#$56dne52_9d1, Rua bla 01 23             ,
+,          2 , Pedro Camargo    , pedro.ribeiro@gmail.com , pedro.ribeiro   , 12@#$0s,k45       , Rua dos Empresarios 90823 ,
+,          7 , Julia Ribeiro    , julia.ribeiro@gmail.com , julia.ribeiro   ,12@#$d83j56       , Rua dos Bla78364 23       ,
+,          8 , Rose Nascimento  , rose@gmail.com          , rose.nascimento , 12@#$0osl9        , Rua dos blablabla 89723   ,
+,          9 , Daniela da Silva , daniela.silva@gmail.com , dani.silva      , 12@#$9skw4        , Rua dos Comerciarios 90   ,
+("muito bem","Comentario"),
+(8,2,"Nao funciona mesmo","Comentario");
+
+insert into fisica(cpf,rg,cod_cliente)
+values 
+(11523078920, 8250780, 1),  
+(19845698211, 2509817, 8),
+
